b96f4fa9a6068a1276fd4c5c3a59c8edd3cfa2a8
fangming <fsmflying@163.com>
2021-01-26 13:50:14 +0800

    initialize project from study

---
+++ io/src/main/java/com/fsmflying/study/quickstart2021/io/Startup.java
@@ -0,0 +1,13 @@
+package com.fsmflying.study.quickstart2021.io;
+
+/**
+ * Hello world!
+ *
+ */
+public class Startup
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
