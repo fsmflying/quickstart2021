d3399acc1b12b0522a9014865d5818a7ca072b50
fangming <fsmflying@163.com>
2021-02-03 10:31:18 +0800

    add file-split module

---
+++ file-split/src/main/java/com/fsmflying/study/quickstart2021/filesplit/Startup.java
@@ -0,0 +1,13 @@
+package com.fsmflying.study.quickstart2021.filesplit;
+
+/**
+ * Hello world!
+ *
+ */
+public class Startup
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
